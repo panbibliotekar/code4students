ac3e2c75b60d7f837fc08fc1c4a3a38cc0913ed3
ПАН БІБЛІОТЕКАР <pan.bibliotekar@gmail.com>
2022-05-31 16:37:20 +0300

    Add files via upload

---
+++ import_csv_example2.py
@@ -0,0 +1,44 @@
+import plotly.graph_objects as go
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv')
+df['text'] = df['airport'] + '' + df['city'] + ', ' + df['state'] + '' + 'Arrivals: ' + df['cnt'].astype(str)
+
+
+fig = go.Figure(data=go.Scattergeo(
+        locationmode = 'USA-states',
+        lon = df['long'],
+        lat = df['lat'],
+        text = df['text'],
+        mode = 'markers',
+        marker = dict(
+            size = 10,
+            opacity = 1,
+            reversescale = True,
+            autocolorscale = False,
+            symbol = 'square',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = 'Blues',
+            cmin = 0,
+            color = df['cnt'],
+            cmax = df['cnt'].max(),
+            colorbar_title="Incoming flights"
+        )))
+
+fig.update_layout(
+        title = 'Most trafficked US airports',
+        geo = dict(
+            scope='usa',
+            projection_type='albers usa',
+            showland = True,
+            landcolor = "rgb(250, 250, 250)",
+            subunitcolor = "rgb(217, 217, 217)",
+            countrycolor = "rgb(217, 217, 217)",
+            countrywidth = 0.5,
+            subunitwidth = 0.5
+        ),
+    )
+fig.show()
